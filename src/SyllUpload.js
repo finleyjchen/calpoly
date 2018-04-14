135d929889b69ae4754743d2f43b9ca2c06b297a
Finley Chen <inlorfaze@gmail.com>
2018-04-14 06:53:12 -0700

    i tried

--- src/SyllUpload.js
+++ src/SyllUpload.js
@@ -1,26 +1,64 @@
 import React, { Component } from 'react';
-import './Card.css';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Document, Page } from 'react-pdf/dist/entry.webpack';
-
-class SyllUpload extends Component {
-    constructor(props) {
-        super(props);
-      }
-
-    render() {
-      return (
-        <div>
-        <Document
-          file="somefile.pdf"
-          onLoadSuccess={this.onDocumentLoad}
-        >
-        <Page />
-        </Document>
-      </div>
-      );
+import Phil126 from './phil126syllabus.pdf';
+import './SyllUpload.css';
+import PDF from 'react-pdf-js';
+ 
+class SyllUpload extends React.Component {
+  state = {};
+ 
+  onDocumentComplete = (pages) => {
+    this.setState({ page: 1, pages });
+  }
+ 
+  onPageComplete = (page) => {
+    this.setState({ page });
+  }
+ 
+  handlePrevious = () => {
+    this.setState({ page: this.state.page - 1 });
+  }
+ 
+  handleNext = () => {
+    this.setState({ page: this.state.page + 1 });
+  }
+ 
+  renderPagination = (page, pages) => {
+    let previousButton = <li className="previous" onClick={this.handlePrevious}><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
+    if (page === 1) {
+      previousButton = <li className="previous disabled"><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
     }
+    let nextButton = <li className="next" onClick={this.handleNext}><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
+    if (page === pages) {
+      nextButton = <li className="next disabled"><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
+    }
+    return (
+      <nav>
+        <ul className="pager">
+          {previousButton}
+          {nextButton}
+        </ul>
+      </nav>
+      );
+  }
+ 
+  render() {
+    let pagination = null;
+    if (this.state.pages) {
+      pagination = this.renderPagination(this.state.page, this.state.pages);
+    }
+    return (
+      <div>
+        <PDF
+          file="./phil126syllabus.pdf"
+          onDocumentComplete={this.onDocumentComplete}
+          onPageComplete={this.onPageComplete}
+          page={this.state.page}
+        />
+        {pagination}
+      </div>
+    )
+  }
 }
+ 
 
 export default SyllUpload;
